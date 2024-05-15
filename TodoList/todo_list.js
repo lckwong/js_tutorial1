3cb474f322f1ffaaf1a20d69cea59ea46da88442
lckwong <leokwong@gmail.com>
2024-05-15 15:08:16 -0400

    update todo list

--- TodoList/todo_list.js
+++ TodoList/todo_list.js
@@ -34,6 +34,11 @@ function clearCompletedTasks() {
     displayTasks();
 }
 
+function clearAllTasks() {
+    tasks = [];
+    displayTasks();
+}
+
 addTaskBtn.addEventListener("click", addTask);
 clearCompletedBtn.addEventListener("click", clearCompletedTasks);
 displayTasks();
